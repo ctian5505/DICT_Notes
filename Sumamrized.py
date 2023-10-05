a3ae0caa62b6013b1ca108a7dfea520f2cabcb66
ctian5505 <66417615+ctian5505@users.noreply.github.com>
2023-10-05 20:06:37 +0800

    Create Sumamrized.py

---
+++ Sumamrized.py
@@ -0,0 +1,19 @@
+# Casting = changing data type
+# String placeholder = f"{}"
+# String Formatting
+  upper()
+  lower()
+  capitalize() = convert the first character to uppercase
+  title() = convert the first character of EACH to uppercase
+  split() = make the string to list
+  replace() = 
+  len() = count the character
+    #Example
+      str1 = "pyhton is Good"
+      print(str1.upper())
+      print(str1.lower())
+      print(str1.capitalize())
+      print(str1.title())
+      print(str1.split())
+      print(str1.replace("is", "is very"))
+      print(len(str1))
